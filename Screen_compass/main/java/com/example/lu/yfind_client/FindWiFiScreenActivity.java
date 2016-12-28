2e563f0b05e1c4050a9faabdfbd4ee9b2c96896d
Lu <r04943083@ntu.edu.tw>
2016-12-28 20:05:04 +0800

    add distanse

--- Screen_compass/main/java/com/example/lu/yfind_client/FindWiFiScreenActivity.java
+++ Screen_compass/main/java/com/example/lu/yfind_client/FindWiFiScreenActivity.java
@@ -38,6 +38,38 @@ public class FindWiFiScreenActivity extends Activity implements SensorEventListe
     private SensorManager mSensorManager;
 
     TextView tvHeading;
+    TextView tvDistanse;
+
+    private final double EARTH_RADIUS = 6378137.0;
+
+    private double gps2m(double lat_a, double lng_a, double lat_b, double lng_b) {
+        double radLat1 = (lat_a * Math.PI / 180.0);
+        double radLat2 = (lat_b * Math.PI / 180.0);
+        double a = radLat1 - radLat2;
+        double b = (lng_a - lng_b) * Math.PI / 180.0;
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+                + Math.cos(radLat1) * Math.cos(radLat2)
+                * Math.pow(Math.sin(b / 2), 2)));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000) / 10000;
+        return s;
+    }
+
+    private double gps2d(double lat_a, double lng_a, double lat_b, double lng_b) {
+        double d = 0;
+        lat_a=lat_a*Math.PI/180;
+        lng_a=lng_a*Math.PI/180;
+        lat_b=lat_b*Math.PI/180;
+        lng_b=lng_b*Math.PI/180;
+
+        d=Math.sin(lat_a)*Math.sin(lat_b)+Math.cos(lat_a)*Math.cos(lat_b)*Math.cos(lng_b-lng_a);
+        d=Math.sqrt(1-d*d);
+        d=Math.cos(lat_b)*Math.sin(lng_b-lng_a)/d;
+        d=Math.asin(d)*180/Math.PI;
+
+        // d = Math.round(d*10000);
+        return d;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
